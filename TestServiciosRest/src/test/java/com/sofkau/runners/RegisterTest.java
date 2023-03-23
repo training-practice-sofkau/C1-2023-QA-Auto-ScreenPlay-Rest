b5fe785bdda5ce8664d329ee6cc9c3218d4e5a89
Nevardo Antonio Ospina Zuñiga <ospina_88@hotmail.com>
2023-03-23 12:36:26 -0500

    Se crea clase consultaPublicacionStepDefinition y un feature

--- TestServiciosRest/src/test/java/com/sofkau/runners/RegisterTest.java
+++ TestServiciosRest/src/test/java/com/sofkau/runners/RegisterTest.java
@@ -7,9 +7,10 @@ import org.junit.runner.RunWith;
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
         snippets = CucumberOptions.SnippetType.CAMELCASE,
-        features = {"src/test/resources/features/rest"},
+        features = {"src/test/resources/features/rest/GetPlaceHolderPublicaciones"},
         glue = {"com.sofkau.stepdefinitons"},
         tags = {""}
+      //src/test/resources/features/rest
 )
 public class RegisterTest {
 }
---
+++ TestServiciosRest/src/test/java/com/sofkau/stepdefinitons/ConsultaPublicacionesStepDefinition.java
@@ -0,0 +1,21 @@
+package com.sofkau.stepdefinitons;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class ConsultaPublicacionesStepDefinition {
+
+    @Given("que el usuario esta en la pagina de registro")
+    public void que_el_usuario_esta_en_la_pagina_de_registro() {
+
+    }
+    @When("el usuario envia una peticion get con el {string}")
+    public void el_usuario_envia_una_peticion_get_con_el(String string) {
+
+    }
+    @Then("el usuario ve un codigo de respuesta estado {int} y la la informacion de la publicacion")
+    public void el_usuario_ve_un_codigo_de_respuesta_estado_y_la_la_informacion_de_la_publicacion(Integer int1) {
+
+    }
+}
