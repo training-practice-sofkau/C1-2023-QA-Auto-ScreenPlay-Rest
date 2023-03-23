5e352af0fe845580e9139e8cd91544ee01a46b74
Juan Esteban Pineda <juan.pineda@sofka.com.co>
2023-03-23 08:54:46 -0500

    se abjunta la base del reto

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# C1-2023-QA-Auto-ScreenPlay-Rest
+# Demo ScreenPlay Rest
+
+"NO quiero ser como antes, Quiero ser MEJOR".
+
+Seleccione 3 peticiones Rest que sean:
+
+- De fuentes diferentes reqres, 
+- Seleccione entre (post, put, patch, get, delete)
+- Incluya comparaciónes (assert) empleando las librerias Json Simple o Gson, y sin librerías (String).
+- En caso de ser posible los escenarios deben ser outline.
+
+Nota: Utilice este repositorio como base para realizar su automatización. Recuerde que un escenario de automatización debe ser independiente, replicable, debe terminar en una aserción y cerrar el driver.
+
+Requisitos:
+
+1. Gestor de dependencias Gradle
+2. Test Runner de Cucumber
+3. Patron ScreenPlay 
+4. Framework Serenity BDD
+5. Versión java 1.8 o superior
---
+++ TestServiciosRest/src/test/java/com/sofkau/stepdefinitons/RegisterStepDefinition.java
@@ -0,0 +1,59 @@
+package com.sofkau.stepdefinitons;
+
+import com.sofkau.models.Response;
+import com.sofkau.models.User;
+import com.sofkau.setup.ApiSetUp;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import net.serenitybdd.rest.SerenityRest;
+import org.apache.http.HttpStatus;
+
+import static com.sofkau.questions.ReturnRegisterSuccessfulJsonResponse.returnRegisterSuccessfulJsonResponse;
+import static com.sofkau.tasks.DoPost.doPost;
+import static com.sofkau.utils.ReqresResources.REGISTER_SUCCESSFUL_RESOURCE;
+import static com.sofkau.utils.ReqresResources.REQRES_BASE_URL;
+import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
+import static net.serenitybdd.screenplay.rest.questions.ResponseConsequence.seeThatResponse;
+import static org.hamcrest.CoreMatchers.notNullValue;
+
+
+public class RegisterStepDefinition extends ApiSetUp {
+    private User user = new User();
+
+    String bodyPrueba = "{\n" +
+            "    \"email\": \"eve.holt@reqres.in\",\n" +
+            "    \"password\": \"pistol\"\n" +
+            "}";
+
+    @Given("the user is in the register page")
+    public void theUserIsInTheRegisterPage() {
+        setUp(REQRES_BASE_URL.getValue());
+    }
+
+    @When("the user send a registration request with the {string} and the {string}")
+    public void theUserSendARegistrationRequestWithTheAndThe(String email, String password) {
+        user.setEmail(email);
+        user.setPassword(password);
+        actor.attemptsTo(
+                doPost()
+                        .withTheResource(REGISTER_SUCCESSFUL_RESOURCE.getValue())
+                        .andTheRequestBody(user)
+        );
+        System.out.println(SerenityRest.lastResponse().body().asString());
+
+    }
+
+    @Then("the user see a status {int} response code and an id with a token")
+    public void theUserSeeAStatusResponseCodeAndAnIdWithAToken(Integer statusCode) {
+        Response actualResponse= returnRegisterSuccessfulJsonResponse().answeredBy(actor);
+        actor.should(
+                seeThatResponse("El codigo de respuesta es: " + HttpStatus.SC_OK,
+                        response -> response.statusCode(statusCode)),
+                seeThat("Retorna información",
+                        act -> actualResponse, notNullValue())
+        );
+
+    }
+
+}
