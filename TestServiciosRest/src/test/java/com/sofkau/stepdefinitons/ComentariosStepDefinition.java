0f49b8c28a21b84a1c9c48bbc6830fba586c0133
nunezjose770 <nunezjose770@gmail.com>
2023-03-23 23:20:32 -0500

    Se agregan las aserciones de cada feature, pendiente arreglar asercion del Put

--- TestServiciosRest/src/test/java/com/sofkau/stepdefinitons/ComentariosStepDefinition.java
+++ TestServiciosRest/src/test/java/com/sofkau/stepdefinitons/ComentariosStepDefinition.java
@@ -4,14 +4,21 @@ import com.sofkau.setup.ApiSetUp;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.restassured.response.Response;
 import net.serenitybdd.rest.SerenityRest;
+import org.apache.http.HttpStatus;
 import org.apache.log4j.Logger;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
+import static com.sofkau.questions.ReturnGetResponse.returnGetResponse;
 import static com.sofkau.tasks.DoGet.doGet;
 import static com.sofkau.utils.JsonPlaceHolderResources.COMMENTS_BY_POST;
 import static com.sofkau.utils.JsonPlaceHolderResources.JSONPLACE_BASE_URL;
+import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
+import static net.serenitybdd.screenplay.rest.questions.ResponseConsequence.seeThatResponse;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.notNullValue;
 
 
 public class ComentariosStepDefinition extends ApiSetUp {
@@ -48,7 +55,19 @@ public class ComentariosStepDefinition extends ApiSetUp {
 
 
     @Then("El usuario debe recibir un respuesta de status {int} y los comentarios del post")
-    public void elUsuarioDebeRecibirUnRespuestaDeStatusYLosComentariosDelPost(Integer int1) {
-
+    public void elUsuarioDebeRecibirUnRespuestaDeStatusYLosComentariosDelPost(Integer code) {
+        try {
+            Response actualResponse = returnGetResponse().answeredBy(actor);
+            //responseBody = (JSONObject) parser.parse(actualResponse.getBody().asString());
+            actor.should(
+                    seeThatResponse("El codigo de respuesta es: " + HttpStatus.SC_OK,
+                            response -> response.statusCode(code)),
+                    seeThat("Retorna información",
+                            act -> actualResponse, notNullValue())
+            );
+            LOGGER.info("Asercion exitosa");
+        } catch (Exception e){
+            LOGGER.warn(e.getMessage());
+        }
     }
 }
--- TestServiciosRest/src/test/java/com/sofkau/stepdefinitons/PutPostStepDefinition.java
+++ TestServiciosRest/src/test/java/com/sofkau/stepdefinitons/PutPostStepDefinition.java
@@ -6,13 +6,20 @@ import com.sofkau.setup.ApiSetUp;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.restassured.response.Response;
+import org.apache.http.HttpStatus;
 import org.apache.log4j.Logger;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
+import static com.sofkau.questions.ReturnGetResponse.returnGetResponse;
 import static com.sofkau.tasks.DoPut.doPut;
 import static com.sofkau.utils.JsonPlaceHolderResources.JSONPLACE_BASE_URL;
 import static com.sofkau.utils.JsonPlaceHolderResources.POSTS_BY_ID_RESOURCE;
+import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
+import static net.serenitybdd.screenplay.rest.questions.ResponseConsequence.seeThatResponse;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.notNullValue;
 
 
 public class PutPostStepDefinition extends ApiSetUp {
@@ -21,6 +28,8 @@ public class PutPostStepDefinition extends ApiSetUp {
     JSONObject responseBody = null;
     JSONParser parser = new JSONParser();
     Posts posts = new Posts();
+
+    private String idPost;
 
     @Given("El usuario se encuentra en la web de JsonPlaceHolder para actualizar los post")
     public void elUsuarioSeEncuentraEnLaWebDeJsonPlaceHolderParaActualizarLosPost() {
@@ -37,22 +46,40 @@ public class PutPostStepDefinition extends ApiSetUp {
                                                                                    String title,
                                                                                    Integer userId,
                                                                                    String body) {
-        posts.setId(id);
-        posts.setTitle(title);
-        posts.setBody(body);
-        posts.setUserId(userId);
-        actor.attemptsTo(
-                doPut()
-                        .withTheResource(POSTS_BY_ID_RESOURCE.getValue() + id)
-                        .andTheRequestBody(posts)
-        );
+        try {
+            idPost = id;
+            posts.setId(id);
+            posts.setTitle(title);
+            posts.setBody(body);
+            posts.setUserId(userId);
+            actor.attemptsTo(
+                    doPut()
+                            .withTheResource(POSTS_BY_ID_RESOURCE.getValue() + id)
+                            .andTheRequestBody(posts)
+            );
+            LOGGER.info("Peticion realizada");
+        }catch (Exception e){
+            LOGGER.warn(e.getMessage());
+        }
 
     }
 
     @Then("El usuario debe recibir un respuesta de status {int} y el post solicitado con su actualizacion")
     public void elUsuarioDebeRecibirUnRespuestaDeStatusYElPostSolicitadoConSuActualizacion(Integer code) {
-
+        try {
+            Response actualResponse = returnGetResponse().answeredBy(actor);
+            responseBody = (JSONObject) parser.parse(actualResponse.getBody().asString());
+            actor.should(
+                    seeThatResponse("El codigo de respuesta es: " + HttpStatus.SC_OK,
+                            response -> response.statusCode(code)),
+                    seeThat("Retorna información",
+                            act -> actualResponse, notNullValue()),
+                    seeThat("Comparar id",
+                            idC -> responseBody.get("id").toString(), equalTo(idPost))
+            );
+            LOGGER.info("Asercion exitosa");
+        } catch (Exception e){
+            LOGGER.warn(e.getMessage());
+        }
     }
-
-
 }
